790cb7cb90db991d068b5f5aa60928d4b825004f
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-06-03 11:10:43 -0300

    :sparkles: feat: Importação do componente Modal no componente Category, adicionei novas funcionalidades :lipstick: e estilizei com Tailwind CSS
    Adicionei o componente Modal para exibir os detalhes de cada notícia ao clicar na imagem. Além disso, fiz melhorias no código do componente Category, adicionando a exibição da data de publicação, a estilização das imagens com efeito de hover e a abertura do Modal com as informações da notícia selecionada.

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,9 +1,12 @@
 'use client'
 import { useEffect, useState } from 'react';
 import { fetchNewsByCategory, GNewsArticle } from '../utils/api/newapi';
+import Modal from '../components/Modal';
 
 export default function Category({ category }: { category: string }) {
     const [news, setNews] = useState<GNewsArticle[]>([]);
+    const [open, setOpen] = useState(false);
+    const [selectedArticle, setSelectedArticle] = useState<GNewsArticle | null>(null);
 
     useEffect(() => {
         async function fetchNews() {
@@ -26,21 +29,49 @@ export default function Category({ category }: { category: string }) {
         });
     }
 
-    return (
-        <ul className="flex flex-wrap gap-2">
-            {news.map((article, index) => (
-                <li key={index} className="relative">
-                    <h3 className="absolute bottom-0 left-0 right-0 mb-0 text-xl bg-black bg-opacity-50 p-2">
-                        {article.title}
-                    </h3>
-                    <img
-                        src={article.image}
-                        className="w-96 h-80 object-cover"
-                        alt={article.title}
-                    />
-                </li>
-            ))}
-        </ul>
+    function handleDate(date: Date): string {
+        const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
+        return new Date(date).toLocaleDateString('pt-BR', options);
+    }
 
+    return (
+        <div>
+            <ul className="flex justify-center flex-wrap gap-2">
+                {news.map((article, index) => (
+                    <li key={index} className="relative">
+                        <h3 className="absolute bottom-0 left-0 right-0 mb-0 text-xl bg-black bg-opacity-50 p-2 z-10">
+                            {article.title}
+                        </h3>
+                        <div
+                            onClick={() => {
+                                setSelectedArticle(article);
+                                setOpen(true);
+                            }}
+                            className="w-96 h-80 cursor-pointer overflow-hidden rounded-xl"
+                        >
+                            <img
+                                src={article.image}
+                                className="w-full h-full object-cover transform transition-transform hover:scale-110"
+                                style={{ transitionDuration: '1s' }}
+                                alt={article.title}
+                            />
+                        </div>
+
+                    </li>
+                ))}
+            </ul>
+            {selectedArticle && (
+                <Modal
+                    isOpen={open}
+                    setOpen={setOpen}
+                    title={selectedArticle.title}
+                    publishedAt={handleDate(new Date(selectedArticle.publishedAt))}
+                    image={selectedArticle.image}
+                    content={selectedArticle.content}
+                    url={selectedArticle.url}
+                    source={{ name: selectedArticle.source.name }}
+                />
+            )}
+        </div>
     );
 }
