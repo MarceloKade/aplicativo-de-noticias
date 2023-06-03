1e53f61904d8ea91b12922d2ef57c847b44b2faf
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-06-03 11:20:12 -0300

    :sparkles: feat: Adicionado o componente Menu a página inicial
    Importei e adicionei o componente Menu a página inicial para exibir o menu de categorias de notícias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import Menu from "@/components/Menu";
+
 export default function Home() {
   return (
-    <main>
-      <div className='flex flex-col items-center pt-24 h-screen'>
-        <h1 className='text-4xl'>Aplicativo de notícias</h1>
-      </div>
-    </main>
-  )
+    <div className="flex justify-center h-screen">
+      <Menu />
+    </div>
+  );
 }
